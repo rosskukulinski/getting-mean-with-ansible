0a658bdea214aeb5652a44a06aafa13b70a08b37
Ross Kukulinski <ross.kukulinski@asti-usa.com>
2014-07-10 16:09:13 -0700

    initial commit

---
+++ ansible/Readme.md
@@ -0,0 +1,10 @@
+You also need to set a couple of environment variables for the Digital Ocean API.
+
+```
+export DO_API_KEY=xxxxxxxxxxxxxxx
+export DO_CLIENT_ID=xxxxxxxxxxxxxxxxxx
+```
+
+
+
+https://github.com/DaftMonk/generator-angular-fullstack
---
+++ app/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('nodeAnsibleApp', [
+  'ngResource',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+      
+    $locationProvider.html5Mode(true);
+  });
